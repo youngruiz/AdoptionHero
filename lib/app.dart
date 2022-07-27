2b8e7685ad897b40b93dc336decd79c2a00fd484
Joseph Christy <jchristy@tillster.com>
2022-07-27 01:12:30 -0700

    Added log in and register account functionality before app is able to be used. Refactored code.

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,9 @@
-import 'package:adoption_hero/widgets/petUpload.dart';
+import 'package:adoption_hero/screens/register.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/db_test.dart';
-
-class DbTestApp extends StatelessWidget {
-  const DbTestApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
@@ -18,28 +16,13 @@ class DbTestApp extends StatelessWidget {
 
         if(snapshot.connectionState == ConnectionState.done){
           return const MaterialApp(
-            title: 'Flutter Demo',
-            home: MyHomePage(),
+            title: 'Adoption Hero',
+            home: RegisterWidget(),
           );
         } else {
           return const Center(child: CircularProgressIndicator());
         }
       }
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  final String widgetTitle = 'Title';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widgetTitle)),
-      body: const DbTest(),
-      floatingActionButton: UploadButton(),
     );
   }
 }
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,89 +1,132 @@
+import 'package:adoption_hero/screens/navigator_scaffold.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:adoption_hero/main.dart';
 import 'package:adoption_hero/screens/login.dart';
 
-class RegisterWidget extends StatelessWidget {
-  RegisterWidget({Key? key}) : super(key: key);
-  TextEditingController _emailField = TextEditingController();
-  TextEditingController _passwordField = TextEditingController();
 
-  static const routeName = 'Register';
+
+class RegisterWidget extends StatefulWidget {
+  const RegisterWidget({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterWidget> createState() => _RegisterWidgetState();
+}
+
+class _RegisterWidgetState extends State<RegisterWidget> {
+  final TextEditingController _emailField = TextEditingController();
+  final TextEditingController _passwordField = TextEditingController();
+
+
+  @override
+  void dispose(){
+    _emailField.dispose();
+    _passwordField.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Adoption Hero',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'WORKS',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                //controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                //controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                // forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                  },
-                )
-            ),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
+    return Scaffold(backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        title: const Icon(Icons.pets_rounded),
+        backgroundColor: Colors.green[100],
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
                   child: const Text(
-                    'Sign in',
+                    'Adoption Hero',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Register',
                     style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: _emailField,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
                   ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextFormField(
+                  obscureText: true,
+                  controller: _passwordField,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              Container(
+                height: 40,
+              ),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Register'),
+                    onPressed: () async {
+                      bool shouldNavigate = await register();
+                      if(shouldNavigate){
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const NavigatorScaffold()));
+                      }
+                    },
+                  )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Already have an account?'),
+                  TextButton(
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const LoginWidget()));
+                    },
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
   }
+
+  Future<bool> register() async {
+  try{
+    await FirebaseAuth.instance
+    .createUserWithEmailAndPassword(email: _emailField.text.trim(), password: _passwordField.text.trim());
+    print("Registered");
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if(e.code == 'weak-password'){
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use'){
+      print('The account already exists for that email.');
+    }
+    return false;
+  } catch (e) {
+    print(e.toString());
+  }
+  return false;
+}
+
 }
