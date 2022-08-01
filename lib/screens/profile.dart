a9cafd8b3ec147a17bd780f240e1d7daf8d2b8a5
Joseph Christy <jchristy@tillster.com>
2022-07-31 19:20:50 -0700

    Profile page populates with user data.

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTabBodyWidget extends StatelessWidget {
@@ -5,46 +7,85 @@ class ProfileTabBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(),
-      padding: const EdgeInsets.all(8.0),
-      color: Colors.green[50],
-      alignment: Alignment.topCenter,
-      child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: <Widget>[
+    return FutureBuilder<QuerySnapshot>(
+      future: getUser(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if(!snapshot.hasData){
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          var profile = snapshot.data!.docs.first;
+          return Container(
+            constraints: BoxConstraints.expand(),
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.green[50],
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: <Widget>[
+      
+                      Padding(
+                      padding: EdgeInsets.all(10), 
+                      child: Container(
+                        height: 350, width: double.infinity, 
+                        child: Image.asset('assets/images/clown.jpg'))
+                      ),
+      
+                      Padding(
+                        padding: EdgeInsets.all(10), 
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
+                          ),
+                          width: double.infinity, 
+                          child: Text("NAME: ${profile['name']}"))
+                      ),
+      
+                      Padding(
+                        padding: EdgeInsets.all(10), 
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
+                          ),
+                          width: double.infinity, 
+                          child: Text("EMAIL: ${profile['email']}"))
+                      ),
 
-                Padding(
-                padding: EdgeInsets.all(10), 
-                child: Container(
-                  height: 350, width: double.infinity, 
-                  child: Image.asset('assets/images/clown.jpg'))
-                ),
+                      Padding(
+                        padding: EdgeInsets.all(10), 
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
+                          ),
+                          width: double.infinity, 
+                          child: Text("USER TYPE: ${profile['userType']}"))
+                      ),
 
-                Padding(
-                  padding: EdgeInsets.all(10), 
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)
-                    ),
-                    width: double.infinity, 
-                    child: Text("USER INFO"))
-                ),
-
-                Padding(
-                  padding: EdgeInsets.all(10), 
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueAccent)
-                    ),
-                    width: double.infinity, 
-                    child: Text("USER INFO"))
-                ),
-
-                ],
-            ),)), 
+                      Padding(
+                        padding: EdgeInsets.all(10), 
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
+                          ),
+                          width: double.infinity, 
+                          child: Text("User Since: ${profile['dateCreated']}"))
+                      ),
+      
+                      ],
+                  ),)), 
+          );
+        }
+      }
     );
   } 
+}
+
+Future<QuerySnapshot> getUser() async {
+  String userEmail = FirebaseAuth.instance.currentUser!.email.toString().trim();
+  print(userEmail);
+  return await FirebaseFirestore.instance
+            .collection("users")
+            .where('email', isEqualTo: userEmail)
+            .get();
 }
